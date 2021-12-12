3c70f4896ace605601629b07f1c8d40f8f745332
Diego Archila <daat3523@gmail.com>
2021-12-12 11:16:41 -0500

    [UPDATE] app.js: se agrega la ruta cat.html en GET

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.get("/", (req, res) => res.sendFile(path.resolve(pathViews, "index.html")));
 app.get("/login", (req, res) => res.sendFile(path.resolve(pathViews, "login.html")));
 app.get("/signup", (req, res) => res.sendFile(path.resolve(pathViews, "signup.html")));
 app.get("/products", (req, res) => res.sendFile(path.resolve(pathViews, "products.html")));
+app.get("/cart", (req, res) => res.sendFile(path.resolve(pathViews, "cart.html")));
 
 app.listen(port, console.log("Server runing at the port",port));
