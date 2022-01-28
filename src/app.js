3141568b8490d18aed718e3fdad0e80e9f600e4e
unknown <felipejaramillolon@gamil.com>
2022-01-28 00:28:50 -0500

    [NEW] Llamada del methodOverride y ruta del 404

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 /* IMPORTS */
 const express=require("express");
 const app=express();
+const methodOverride = require('method-override');
+
 const indexRouter=require("./routes/mainRouts.js");
 const cartRouter=require('./routes/cartRouts.js');
+
 
 /* Settings */
 const PORT=process.env.PORT || 3003;
@@ -13,11 +16,19 @@ app.set("view engine", "ejs");
 app.set("views", "./src/views/");
 
 app.use(express.urlencoded({ extended:false }));
+app.use(express.json())
 
 /* Routes Asignations */
 app.use(indexRouter);
 app.use(cartRouter)
 
+/* 404 */
+app.use((req, res, next) => {
+	res.status(404).render('not-found');
+})
+
+/* Funcionalidad del put y delete */
+app.use(methodOverride('_method')) 
 
 /* Start Server */
 app.listen(PORT, () => { 
