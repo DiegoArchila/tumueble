84591c05f29d3a671945672e14d93342adb631e7
Diego Archila <daat3523@gmail.com>
2022-02-03 12:58:26 -0500

    [NEW] Se agrega el carrito de compras

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,3 +1,25 @@
+/**Index contain information from title the page.
+ * 
+ * index {
+ *  name: name of the project
+ *  title: title of the page
+ * }
+*/
+const index=require("../databases/index.json");
+index.title="Cart";
+
+/**settingGeneralSite contain information from the struct of the head.
+ * 
+ * settingGeneralSite {
+ *  languaje: lanjuaje of the site (See the settingGeneralSite.json)
+ *  title: title of the page
+ * }
+*/
+const settingGeneral=require("../databases/settingGeneralSite.json");
+
 module.exports= {
-    cart: (req,res)=> res.render('cart.ejs')
-}
+    cart: (req,res)=> res.render("cart.ejs", {
+        index,
+        settingGeneral
+    })
+};
