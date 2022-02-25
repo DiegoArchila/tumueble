f18fbdf0a43b64f254bde0fa371d55caa3a67229
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-02-24 21:59:47 -0500

    Se crea el diseño de las cards de los productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,14 @@
-const dirDatabases="../databases/";
-const settingGeneral= require("../databases/settingGeneralSite.json");
-const index= require("../databases/index.json");
+const dirDatabases = "../databases/";
+const settingGeneral = require("../databases/settingGeneralSite.json");
+const index = require("../databases/index.json");
+const products = require("../databases/business/products.json");
 
-module.exports= {
-    index: (req,res) => {
-        res.render("index.ejs", {
-            settingGeneral,
-            index
-        })
-    }
-}
+module.exports = {
+  index: (req, res) => {
+    res.render("index.ejs", {
+      settingGeneral,
+      index,
+      products,
+    });
+  },
+};
