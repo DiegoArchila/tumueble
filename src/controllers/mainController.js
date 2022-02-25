b70d7f54144851f10815f154345720b028753714
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-02-25 00:06:11 -0500

    Slider de productos mas comprados en el home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,10 +5,22 @@ const products = require("../databases/business/products.json");
 
 module.exports = {
   index: (req, res) => {
+    let cantidadSlider = 3;
+    let productsMasCompradosOrdenados = products.sort(
+      (a, b) => b.buyes - a.buyes
+    );
+    let productsMasComprados = [];
+    for (
+      let i = 0;
+      i < cantidadSlider && i < productsMasCompradosOrdenados.length;
+      i++
+    ) {
+      productsMasComprados.push(productsMasCompradosOrdenados[i]);
+    }
     res.render("index.ejs", {
       settingGeneral,
       index,
-      products,
+      productsMasComprados,
     });
   },
 };
